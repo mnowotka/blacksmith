88ba68454d2992c41f2f07452cd217696265d2e3
Michał Nowotka <mmmnow@gmail.com>
2019-06-02 12:03:17 +0200

    Update WeaponTest.java

--- src/test/java/WeaponTest.java
+++ src/test/java/WeaponTest.java
@@ -3,10 +3,7 @@ import junit.framework.TestCase;
 public class WeaponTest extends TestCase {
 
     public void testFactory(){
-        WeaponFactory elfFactory = new WeaponFactory(new ElfBlacksmith());
-        Weapon elfSward = elfFactory.buildWeapon(WeaponType.SWORD);
-        WeaponFactory orcFactory = new WeaponFactory(new OrcBlacksmith());
-        Weapon orcSward = orcFactory.buildWeapon(WeaponType.SWORD);
+
     }
 
 }
